9a29b23e3f632a89a132f192250613ef71b6a83c
Sixto Guerra <sixto.guerra1982@gmail.com>
2020-10-27 22:28:47 -0300

    model category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,20 @@
 class Category < ApplicationRecord
     before_save :category_id_validation
+
     def category_id_validation
-        false
+        if self.category_id != 0
+            padre = Category.find_by(id: self.category_id)
+            if padre == nil
+                raise "Padre no Encontrado"
+            else
+                if padre.category_id != 0
+                    raise "Padre a asignar, no corresponde a Padre"
+                end
+            end
+        end
+    end
+
+    def father?
+        self.category_id == 0
     end
 end
